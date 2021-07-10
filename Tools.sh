d08a8b233ca4bf31e1e5e7d711d8e7cf94a76714
Shephard177 <87244769+Shephard177@users.noreply.github.com>
2021-07-10 22:30:26 +0000

    Update Tools.sh

--- Tools.sh
+++ Tools.sh
@@ -1,3 +1,6 @@
+# Main automation script
+# By Shephard 10/07/2021
+
 # Updates system before installing Tools
 sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
 
