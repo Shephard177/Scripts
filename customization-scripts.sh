01d46dbbcd7a29a173a186c2f6e0478b9cd1afd3
Shephard177 <87244769+Shephard177@users.noreply.github.com>
2021-07-10 22:29:58 +0000

    Update customization-scripts.sh

--- customization-scripts.sh
+++ customization-scripts.sh
@@ -1,13 +1,12 @@
-
+# Main automation script
+# By Shephard 10/07/2021
 
 echo "Downloading Tools..."
 ./tools.sh
 echo "Configuring Prompt..."
 ./prompt.sh
 echo "Configuring Vim..."
-vim.sh
-echo "Configuring Tmux..."
-tmux.sh
+#vim.sh
+#echo "Configuring Tmux..."
+#tmux.sh
 echo "Done!"
-
-
