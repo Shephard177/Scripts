807bd6ff33921112054af86f31a53ef69f9c0ecf
Shephard177 <87244769+Shephard177@users.noreply.github.com>
2021-07-10 22:41:03 +0000

    Delete prompt.sh

--- prompt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Main automation script
-# By Shephard 10/07/2021
-
-#### Make a backup of the .bashrc file
-cp ~/.bashrc ~/.bashrc.bak
-
-#### Customize bash prompt
-echo 'export PS1="-[\[$(tput sgr0)\]\[\033[38;5;10m\]\d\[$(tput sgr0)\]-\[$(tput sgr0)\]\[\033[38;5;10m\]\t\[$(tput sgr0)\]]-[\[$(tput sgr0)\]\[\033[38;5;214m\]\u\[$(tput sgr0)\]@\[$(tput sgr0)\]\[\033[38;5;196m\]\h\[$(tput sgr0)\]]-\n-[\[$(tput sgr0)\]\[\033[38;5;33m\]\w\[$(tput sgr0)\]]\\$ \[$(tput sgr0)\]"' >> ~/.bashrc
